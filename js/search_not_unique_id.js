319a055d069501af34b410af87dd87e973e5b74c
yamamoto.hiroya <yamamoto.hiroya@hamee.co.jp>
2017-01-17 09:28:31 +0900

    jsの本体の処理を記述

--- js/search_not_unique_id.js
+++ js/search_not_unique_id.js
@@ -1,5 +1,21 @@
 $(function(){
   $('#search_not_unique_id').on('click', funcion(){
-    console.log("js call");
+    var ids = [];
+    // 画面の全要素に対してループを回す（やや遅い可能性あり）
+    $.each($("*"), function(){
+      var id = $(this).attr("id");
+      if(typeof(id) !== "undefined"){
+        // undefined以外の全idを配列に保持
+        ids.push(id);
+      }
+    });
+    /**
+     * 重複したもののみを検出する
+     * @see http://qiita.com/cocottejs/items/7afe6d5f27ee7c36c61f
+     */
+    var not_unique = ids.filter(function (x, i, self) {
+      return self.indexOf(x) === i && i !== self.lastIndexOf(x);
+    });
+    console.log(not_unique);
   });
 });
